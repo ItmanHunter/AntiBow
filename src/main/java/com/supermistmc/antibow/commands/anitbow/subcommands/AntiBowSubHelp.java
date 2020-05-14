c410c4df5f3f6f5cdb910259826e4dca39e8bcf8
ItmanHunter <ItmanHunter@outlook.com>
2020-05-14 20:08:02 +0530

    added more locale messages

--- src/main/java/com/supermistmc/antibow/commands/anitbow/subcommands/AntiBowSubHelp.java
+++ src/main/java/com/supermistmc/antibow/commands/anitbow/subcommands/AntiBowSubHelp.java
@@ -1,9 +1,12 @@
 package com.supermistmc.antibow.commands.anitbow.subcommands;
 
 import com.supermistmc.antibow.AntiBow;
+import com.supermistmc.antibow.Locale;
+import com.supermistmc.antibow.Permissions;
 import com.supermistmc.antibow.commands.AbstractSubCommand;
 import com.supermistmc.antibow.services.locale.ILocaleService;
 import com.supermistmc.antibow.services.locale.LocaleService;
+import com.supermistmc.antibow.services.region.RegionService;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -17,8 +20,12 @@ public class AntiBowSubHelp extends AbstractSubCommand {
     @Override
     public boolean executeCommand(CommandSender sender, Command command, String label, String[] args) {
         ILocaleService localeService = LocaleService.getILocaleService();
-        for(String message:localeService.getHelpString()) {
-            sender.sendMessage(message);
+        if (sender.isOp() || sender.hasPermission(Permissions.HELP)) {
+            for(String message:localeService.getHelpString()) {
+                sender.sendMessage(message);
+            }
+        } else {
+            sender.sendMessage(localeService.getLocale(Locale.NO_PERMISSION));
         }
         return true;
     }
